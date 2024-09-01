a28275013bae668840f8abdb86e1cedf5e8c3761
Ethan <ethanftracey@gmail.com>
2024-09-01 15:03:04 -0230

    Added ResidentCard component and filled the cards with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import './App.css'
 import ResidentCard from './components/ResidentCard'
 
+const data = { 
+	residents: [
+		{
+			name: "bill",
+			age: "65",
+			wing: "Left",
+			room: "100"
+		},
+		{
+			name: "jill",
+			age: "50",
+			wing: "Left",
+			room: "101"
+		},
+		{
+			name: "billy",
+			age: "93",
+			wing: "Right",
+			room: "102"
+		},
+	]
+}
+
+const listDummyCards = data.residents.map(resident => 
+	<ResidentCard name={resident.name} age={resident.age} info={[resident.wing, resident.room]}/>
+)
+
 function App() {
 
   return (
     <>
-			<ResidentCard />
+			{listDummyCards}
     </>
   )
 }
--- src/components/ResidentCard.tsx
+++ src/components/ResidentCard.tsx
@@ -7,18 +7,20 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-export default function ResidentCard() {
+export default function ResidentCard( {name, age, info} : {name: string, age: string, info: [string, string]} ) {
 	return (
 		<Card>
 			<CardHeader>
-				<CardTitle>Card Title</CardTitle>
+				<CardTitle>Resident Name: {name}</CardTitle>
 				<CardDescription>Card Description</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<p>Card Content</p>
+				<p>Age</p>
+				<p>{age}</p>
 			</CardContent>
 			<CardFooter>
-				<p>Card Footer</p>
+				<p>Wing - {info[0]} | Room - {info[1]}</p>
+				
 			</CardFooter>
 		</Card>
 	)
