82ebb7314a1bf94ddfe098c807876d7d4b8535b7
Ethan <ethanftracey@gmail.com>
2024-09-02 21:30:06 -0230

    Added some styling to the cards

--- src/components/ResidentCard.tsx
+++ src/components/ResidentCard.tsx
@@ -1,28 +1,33 @@
 // TODO: think of a cool way to display the resident card
+// TODO: make hover effect on the card title better
 import {
   Card,
   CardContent,
-  CardDescription,
+//   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import pic from '../assets/test-stock-img.jpg' 
+import pic from '../assets/test-stock-img.jpg' // stock image
 import { useState } from "react"
 
 export default function ResidentCard( {name, info} : {name: string, info: [string, string]} ) {
-	const [isCollapsed, setIsCollapsed] = useState(true)
+	const [isCollapsed, setIsCollapsed] = useState(true);
 
 	const handleClick = () => {
-		setIsCollapsed(!isCollapsed)
-		console.log(isCollapsed)
+		setIsCollapsed(!isCollapsed);
 	}
 
 	return (
-		<Card>
+		<Card className={`grow-0 shadow-lg rounded-lg m-2 
+		${isCollapsed ? 'bg-white h-30' : 'bg-sky-200 h-60'}
+		`}>
 			<CardHeader className="text-center" onClick={handleClick}>
-				<CardTitle>{name}</CardTitle>
-				{/* <CardDescription>Card Description</CardDescription> */}
+				<CardTitle className={`hover:bg-sky-600 hover:text-white cursor-pointer width-auto p-2 rounded-lg border-2
+					${isCollapsed ? 'text-blue-600' : 'bg-sky-600 text-white bold underline'}
+				`}>
+					{name}
+				</CardTitle>
 			</CardHeader>
 			{!isCollapsed && (
 				<CardContent className="flex items-center justify-center">
