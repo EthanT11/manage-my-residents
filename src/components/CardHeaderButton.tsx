339b64764edda795c2ab84622b929dd733742d7e
Ethan <ethanftracey@gmail.com>
2024-09-05 15:38:08 -0230

    Fixed ref issues from addResDialog Refactored the code in CardHeader to use forwardRef

--- src/components/CardHeaderButton.tsx
+++ src/components/CardHeaderButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button";
+import React from "react";
 
 interface CardHeaderButtonProps {
 	text: string; 
@@ -7,16 +8,33 @@ interface CardHeaderButtonProps {
 }
 // TODO: Add custom styling for the button
 // TODO: Add a prop or something to change if the button is disabled by default
+// NOTE: until I'm more comfortable with how ref fully works https://react.dev/reference/react/forwardRef
+// --------------------------------------Old code-----------------------------------------------
+// export default function CardHeaderButton({text, onClick, variant}: CardHeaderButtonProps) {
+//   return (
+//     <>
+// 	  	<Button 
+// 			className={`bg-white hover:bg-blue-600 hover:text-white ${variant}`}
+// 			onClick={onClick}
+// 		>
+// 			{text}
+// 		</Button>
+//     </>
+//   );
+// }
 
-export default function CardHeaderButton({text, onClick, variant}: CardHeaderButtonProps) {
-  return (
-    <>
-	  	<Button 
-			className={`bg-white hover:bg-blue-600 hover:text-white ${variant}`}
-			onClick={onClick}
+const CardHeaderButton = React.forwardRef<HTMLButtonElement, CardHeaderButtonProps>( // typescript <HTMLButtonElement> is the type of the ref | <CardHeaderButtonProps> is the type of the props
+	({ text, onClick, variant}, ref) => {		 // takes two args props and ref 									
+	  return (
+		<Button 
+		  ref={ref} // ref is passed to the button element
+		  className={`bg-white hover:bg-blue-600 hover:text-white ${variant}`}
+		  onClick={onClick}
 		>
-			{text}
+		  {text}
 		</Button>
-    </>
-  );
-}
+	  );
+	}
+);
+
+export default CardHeaderButton;
