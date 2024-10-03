e64564920685fdc9f822a2943b45560001af1d60
Ethan <ethanftracey@gmail.com>
2024-10-03 20:12:10 -0230

    feat(Account.tsx): Added ability to fetch profile and match to logged in user. Added fetchProfileData to connect to Supabase and fetch information from the profiles table and match it with the logged in user.

--- src/components/pages/Account.tsx
+++ src/components/pages/Account.tsx
@@ -3,6 +3,7 @@ import { User } from '@supabase/supabase-js';
 import { TopNavBar } from "../Common";
 import { useNavigate } from "react-router-dom";
 import useSupabase from "@/hooks/useSupabase";
+import { supabase } from "@/supabaseClient";
 
 export default function Account() {
 	const [user, setUser] = useState<User | null>(null);
@@ -17,6 +18,23 @@ export default function Account() {
 			}
 			setUser(user);
 		});
+
+		const fetchProfileData = async () => {
+			const { data, error, count } = await supabase
+				.from('profiles') // from profiles table
+				.select('*', { count: 'exact' }) // select all columns and count
+				.eq('user_id', user?.id) // where "user_id" is equal to the user's id
+				.single(); // get the first result
+			if (error) {
+				console.error('Error fetching profile data:', error.message);
+			} else {
+				console.log('Profile data:', data);
+				console.log('User:', user?.id);
+				console.log('Count:', count);
+			
+			}
+		}
+		fetchProfileData();
 	  }, []);
 	return (
 		<div>
@@ -25,6 +43,7 @@ export default function Account() {
 			{user ? (
 				<div>
 					<h1>Welcome, {user.email}</h1>
+					<h2></h2>
 				</div>
 			) : (
 				<h1>Not signed in</h1>
