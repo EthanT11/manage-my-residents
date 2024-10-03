994f13090dda3a24d1f1d35ed339fc92f1e59172
Ethan <ethanftracey@gmail.com>
2024-10-03 20:47:35 -0230

    fix(Account): Seperated fetchProfileData and fetchUser into their own useEffects. Did this to stop a loop from occuring, and also added some better error handing. Started taking data from the profile tables and displaying them on the page

--- src/components/pages/Account.tsx
+++ src/components/pages/Account.tsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
-import { User } from '@supabase/supabase-js';
-import { TopNavBar } from "../Common";
 import { useNavigate } from "react-router-dom";
-import useSupabase from "@/hooks/useSupabase";
+import { User } from '@supabase/supabase-js';
 import { supabase } from "@/supabaseClient";
+import useSupabase from "@/hooks/useSupabase";
+import { TopNavBar } from "../Common";
 
 export default function Account() {
 	const [user, setUser] = useState<User | null>(null);
 	const navigate = useNavigate();
 	const { fetchUser } = useSupabase();
+	const [firstName, setFirstName] = useState(null);
+	const [lastName, setLastName] = useState(null);
+	const [home, setHome] = useState(null);
+	const [position, setPosition] = useState(null);
 
 	useEffect(() => {
 		fetchUser().then(({ user }) => {
@@ -19,31 +23,45 @@ export default function Account() {
 			setUser(user);
 		});
 
-		const fetchProfileData = async () => {
-			const { data, error, count } = await supabase
-				.from('profiles') // from profiles table
-				.select('*', { count: 'exact' }) // select all columns and count
-				.eq('user_id', user?.id) // where "user_id" is equal to the user's id
-				.single(); // get the first result
-			if (error) {
-				console.error('Error fetching profile data:', error.message);
-			} else {
-				console.log('Profile data:', data);
-				console.log('User:', user?.id);
-				console.log('Count:', count);
-			
-			}
-		}
-		fetchProfileData();
-	  }, []);
+	}, []); 
+	
+	useEffect(() => {
+		  const fetchProfileData = async () => {
+			  if (!user?.id) {
+				  console.error('User not found');
+				  return;
+			  }
+
+			  const { data, error, count } = await supabase
+				  .from('profiles') // from profiles table
+				  .select('*', { count: 'exact' }) // select all columns and count
+				  .eq('user_id', user.id) // where "user_id" is equal to the user's id
+				  .single() // get the first result
+			  if (error) {
+				  console.error('Error fetching profile data:', error.message);
+			  } else if (count !== 1) { // If there is no profile for the user
+				  console.error('Profile not found');
+			  } else {
+				  console.log('Profile data:', data);
+				  setFirstName(data['first_name']);
+				  setLastName(data['last_name']);
+				  setHome(data['home_name']);
+				  setPosition(data['position']);
+			  }
+		  }
+		  if (user) {
+			  fetchProfileData();
+		  }
+
+	  }, [user]);
 	return (
 		<div>
 			<TopNavBar />
 			<h1>Account</h1>
 			{user ? (
 				<div>
-					<h1>Welcome, {user.email}</h1>
-					<h2></h2>
+					<h1>Welcome, {firstName + " " + lastName}</h1>
+					<h2>Home: {home} - Position: {position}</h2>
 				</div>
 			) : (
 				<h1>Not signed in</h1>
