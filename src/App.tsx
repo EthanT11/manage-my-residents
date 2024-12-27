d65b53acfa18fd8b8a0e04f473d43415fe992dea
Ethan <ethanftracey@gmail.com>
2024-12-27 17:58:32 -0330

    Feat(ResidentContext): This was a big commit. Partly due to the big changes this one added. Most Resident parts were interwived so I didn't want to commit half done things just incase something really broke. Resident Context stores information about Residents, ResidentList now updates when a resident is added, ResidentDetails now updates accordingly using the context.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from './hooks/useTheme'
 import { AuthProvider } from './contexts/AuthContext'
 import { UserProvider } from './contexts/UserContext'
+import { ResidentProvider } from './contexts/ResidentContext'
 
 function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
         <UserProvider>
-          <RouterProvider router={router} />
+          <ResidentProvider>
+            <RouterProvider router={router} />
+          </ResidentProvider>
         </UserProvider>
       </AuthProvider>
     </ThemeProvider>
